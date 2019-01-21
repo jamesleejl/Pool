edd495812ade331a1510a967da74a4802125d0bb
James Lee <jameslee.jl+jamesleejl@gmail.com>
2019-01-20 16:13:16 -0800

    Check in for pool app.

--- pool.cpp
+++ pool.cpp
@@ -1,10 +1,14 @@
-// my first program in C++
 #include <iostream>
 #include <vector>
 #include <set>
+#include <chrono>
+#include <ctime>
+
 #include "Eigen/Dense"
 using namespace Eigen;
 using namespace std;
+
+// TODO: Handle bank shots.
 
 /**
  * Whether or not a shot is obstructed and whether it can become unobstructed.
@@ -12,7 +16,7 @@ using namespace std;
 struct obstructions
 {
   // Whether the shot is impossible by non-player balls or not.
-  bool has_permanent_obstruction;
+  bool has_permanent_obstruction = false;
   // The set indices of of player object balls obstructing the shot.
   set<unsigned char> obstructing_object_balls;
 };
@@ -28,23 +32,29 @@ std::ostream &operator<<(std::ostream &o, const obstructions &obstructions)
   return o;
 }
 
-// TODO: Unused.
-struct end_location
+struct shot_path
 {
-  // The percentage chance of ending at this location.
-  unsigned char percent_chance;
-  // The coordinates representing this location.
-  Vector2d coordinates;
+  // The line segments that make up the shot path.
+  vector<Vector2d> path_segments;
+  // The obstructions along this path.
+  obstructions obstructions;
+  // The final location of the cue ball along this path.
+  Vector2d final_position;
 };
 
 // The possible information with regards to a shot
-// TODO: Unused.
 struct shot_info
 {
-  float difficulty;                            // TODO: Make this more efficient.
-  set<unsigned char> obstructing_object_balls; // The object balls owned by the player that are obstructing the shot.
-  obstructions obstructions;                   // THe obstructions for this shot.
+  float difficulty = 0;      // TODO: Make this more efficient.
+  obstructions obstructions; // THe obstructions for this shot.
 };
+
+std::ostream &operator<<(std::ostream &o, const shot_info &shot_info)
+{
+  o << shot_info.difficulty << endl;
+  o << shot_info.obstructions << endl;
+  return o;
+}
 
 // The length of a diamond in units.
 const unsigned char DIAMOND_LENGTH = 2;
@@ -56,8 +66,14 @@ const unsigned char WIDTH = DIAMOND_LENGTH * 4;
 const unsigned char LENGTH = WIDTH * 2;
 // The number of pockets.
 const unsigned char NUM_POCKETS = 6;
+// The number of shot strengths to consider
+const unsigned char NUM_STRENGTHS = 12;
+// The number of shot spins to consider
+const unsigned char NUM_SPINS = 5;
 // The index number of the 8 ball.
 const unsigned char EIGHT_BALL_INDEX = 7;
+// The maximum difficulty of shot to consider shooting.
+const unsigned char MAX_DIFFICULTY_SHOT_TO_CONSIDER = 200;
 // The positions of the pockets.
 vector<Vector2d> pockets;
 // The position of the eight ball.
@@ -69,16 +85,26 @@ vector<Vector2d> object_balls;
 // The initial position of our opponent's object balls.
 vector<Vector2d> opponent_object_balls;
 
-// A table populating the difficulty of making the 8 ball shot with the cue ball at any of the
-// (WIDTH, LENGTH) positions.
-// TODO: Unused.
-shot_info cue_ball_eight_ball_shot_info[WIDTH][LENGTH];
-
 /**
  * A table of obstructions from each object ball index to a given pocket. Includes the 8 ball at index
- * EIGHT_BALL_INDEX.
+ * EIGHT_BALL_INDEX. This is only used to help populate shot_obstructions.
  */
-obstructions ball_to_pocket_obstructions[8][NUM_POCKETS];
+obstructions ball_to_pocket_obstructions_table[EIGHT_BALL_INDEX + 1][NUM_POCKETS];
+
+/**
+ * A table of ghost ball positions for a given object ball index to a pocket.
+ */
+Vector2d ghost_ball_position_table[EIGHT_BALL_INDEX + 1][NUM_POCKETS];
+
+/**
+ * A table of shot infos including obstruction information and difficulty.
+ */
+shot_info shot_info_table[WIDTH][LENGTH][EIGHT_BALL_INDEX + 1][NUM_POCKETS];
+
+/**
+ * A table of possible paths given a cue ball, object ball, pocket, strength, and spin.
+ */
+shot_path shot_path_table[WIDTH][LENGTH][EIGHT_BALL_INDEX + 1][NUM_POCKETS][NUM_STRENGTHS][NUM_SPINS];
 
 /**
  * Initialize the pockets as a vector of Vector2ds.
@@ -91,22 +117,6 @@ void initialize_pockets()
   pockets.push_back(Vector2d(WIDTH, WIDTH));
   pockets.push_back(Vector2d(0, LENGTH));
   pockets.push_back(Vector2d(WIDTH, LENGTH));
-}
-
-/**
- * Set the position of the various balls.
- * TODO: Don't hardcode this.
- */
-void initialize_layout()
-{
-  eight_ball = Vector2d(DIAMOND_LENGTH * 2, DIAMOND_LENGTH * 3);
-  cue_ball = Vector2d(0.15, DIAMOND_LENGTH * 4);
-  for (int i = 0; i < 7; ++i)
-  {
-    object_balls.push_back(Vector2d(DIAMOND_LENGTH * 1 + DIAMOND_LENGTH * 0.25 * i, DIAMOND_LENGTH * 4.15));
-    opponent_object_balls.push_back(Vector2d(DIAMOND_LENGTH * 0.25 * i, DIAMOND_LENGTH * 3));
-  }
-  object_balls[3] = Vector2d(0, DIAMOND_LENGTH * 4);
 }
 
 /**
@@ -169,7 +179,7 @@ bool ball_intersects_segment(Vector2d ball, Vector2d segment_start, Vector2d seg
 obstructions get_obstructions_on_segment_for_shot(unsigned char object_ball_index, Vector2d segment_start, Vector2d segment_end)
 {
   obstructions obstructions;
-  for (int i = 0; i < opponent_object_balls.size(); ++i)
+  for (unsigned char i = 0; i < opponent_object_balls.size(); ++i)
   {
     if (ball_intersects_segment(opponent_object_balls[i], segment_start, segment_end))
     {
@@ -182,7 +192,7 @@ obstructions get_obstructions_on_segment_for_shot(unsigned char object_ball_inde
     obstructions.has_permanent_obstruction = true;
     return obstructions;
   }
-  for (int i = 0; i < object_balls.size(); ++i)
+  for (unsigned char i = 0; i < object_balls.size(); ++i)
   {
     if (i == object_ball_index)
     {
@@ -196,23 +206,204 @@ obstructions get_obstructions_on_segment_for_shot(unsigned char object_ball_inde
   return obstructions;
 }
 
-void populate_cue_ball_eight_ball_shot_info()
+void populate_ball_to_pocket_obstructions_table()
 {
-  for (int p = 0; p < pockets.size(); ++p)
+  for (unsigned char p = 0; p < pockets.size(); ++p)
   {
-  }
-  obstructions obstructing_balls = get_obstructions_on_segment_for_shot(EIGHT_BALL_INDEX, pockets[0], pockets[1]);
-  for (int w = 0; w < WIDTH; ++w)
-  {
-    for (int l = 0; l < LENGTH; ++l)
+    Vector2d pocket = pockets[p];
+    for (unsigned char o = 0; o < object_balls.size(); ++o)
     {
+      ball_to_pocket_obstructions_table[o][p] = get_obstructions_on_segment_for_shot(o, object_balls[o], pocket);
+    }
+    ball_to_pocket_obstructions_table[EIGHT_BALL_INDEX][p] = get_obstructions_on_segment_for_shot(EIGHT_BALL_INDEX, eight_ball, pocket);
+  }
+}
+
+Vector2d get_ghost_ball_for_shot(Vector2d ball, Vector2d pocket)
+{
+  Vector2d pocket_to_ball = pocket - ball;
+  return ball + pocket_to_ball.normalized() * BALL_DIAMETER;
+}
+
+void populate_ghost_ball_position_table()
+{
+  for (unsigned char p = 0; p < pockets.size(); ++p)
+  {
+    for (unsigned char o = 0; o < object_balls.size(); ++o)
+    {
+      ghost_ball_position_table[o][p] = get_ghost_ball_for_shot(object_balls[o], pockets[p]);
+    }
+    ghost_ball_position_table[EIGHT_BALL_INDEX][p] = get_ghost_ball_for_shot(eight_ball, pockets[p]);
+  }
+}
+
+void populate_shot_info_table_obstructions()
+{
+  for (unsigned char o = 0; o < EIGHT_BALL_INDEX + 1; ++o)
+  {
+    for (unsigned char p = 0; p < pockets.size(); ++p)
+    {
+      obstructions ball_to_pocket_obstructions = ball_to_pocket_obstructions_table[o][p];
+      Vector2d ghost_ball_position = ghost_ball_position_table[o][p];
+      for (unsigned char w = 0; w < WIDTH; ++w)
+      {
+        for (unsigned char l = 0; l < LENGTH; ++l)
+        {
+          shot_info &shot_info = shot_info_table[w][l][o][p];
+          shot_info.obstructions = ball_to_pocket_obstructions;
+          if (ball_to_pocket_obstructions.has_permanent_obstruction)
+          {
+            continue;
+          }
+          Vector2d cue_ball = Vector2d(w, l);
+          obstructions cue_to_object_obstructions = get_obstructions_on_segment_for_shot(o, cue_ball, ghost_ball_position);
+          if (cue_to_object_obstructions.has_permanent_obstruction)
+          {
+            shot_info.obstructions.has_permanent_obstruction = true;
+            shot_info.obstructions.obstructing_object_balls.clear();
+            continue;
+          }
+          for (unsigned char index : cue_to_object_obstructions.obstructing_object_balls)
+          {
+            shot_info.obstructions.obstructing_object_balls.insert(index);
+          }
+        }
+      }
     }
   }
 }
 
+/**
+ * Gets the shot difficulty given a cue ball, ghost ball location, and a pocket. It does this by
+ * projecting the cue ball onto the ghost ball and pocket line. It then multiplies the projected
+ * distance by the distance of the ghost ball to the pocket. Represents the shot difficulty
+ * stated here: http://www.sfbilliards.com/articles/1994.pdf
+ * If impossible, it returns inf.
+ */
+float get_shot_difficulty(Vector2d cue_ball, Vector2d ghost_ball, Vector2d pocket)
+{
+  Vector2d ghost_ball_to_pocket = pocket - ghost_ball;
+  Vector2d ghost_ball_to_cue_ball = cue_ball - ghost_ball;
+  float projected_distance_of_cue_ball_to_shot_line = ghost_ball_to_cue_ball.squaredNorm() * ghost_ball_to_pocket.norm() / ghost_ball_to_pocket.dot(ghost_ball_to_cue_ball);
+  if (projected_distance_of_cue_ball_to_shot_line > 0)
+  {
+    return std::numeric_limits<float>::infinity();
+  }
+  return -1 * projected_distance_of_cue_ball_to_shot_line * ghost_ball_to_pocket.norm();
+}
+
+void populate_shot_info_table_difficulty()
+{
+  for (unsigned char o = 0; o < EIGHT_BALL_INDEX + 1; ++o)
+  {
+    for (unsigned char p = 0; p < pockets.size(); ++p)
+    {
+      Vector2d ghost_ball_position = ghost_ball_position_table[o][p];
+      for (unsigned char w = 0; w < WIDTH; ++w)
+      {
+        for (unsigned char l = 0; l < LENGTH; ++l)
+        {
+          shot_info &shot_info = shot_info_table[w][l][o][p];
+          shot_info.difficulty = get_shot_difficulty(Vector2d(w, l), ghost_ball_position, pockets[p]);
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Gets the unit tangent line vector for a given cue ball, ghost ball, and pocket.
+ * TODO: This function needs to be modified to specify the direction the cue ball is traveling
+ * relative to the shot path to determine the follow or draw path for further calculations.
+ */
+Vector2d get_tangent_line(Vector2d cue_ball, Vector2d ghost_ball, Vector2d pocket)
+{
+  Vector2d ghost_ball_to_cue_ball = cue_ball - ghost_ball;
+  Vector2d ghost_ball_to_pocket = pocket - ghost_ball;
+  float determinant = ghost_ball_to_cue_ball.x() * ghost_ball_to_pocket.y() - ghost_ball_to_cue_ball.y() * ghost_ball_to_pocket.x();
+  Vector2d tangent = ghost_ball_to_pocket.unitOrthogonal();
+  if (determinant < 0)
+  {
+    tangent = Vector2d(-1 * tangent.x(), -1 * tangent.y());
+  }
+  return tangent;
+}
+
+void populate_shot_path_table()
+{
+  for (unsigned char o = 0; o < EIGHT_BALL_INDEX + 1; ++o)
+  {
+    for (unsigned char p = 0; p < pockets.size(); ++p)
+    {
+      Vector2d ghost_ball_position = ghost_ball_position_table[o][p];
+      for (unsigned char w = 0; w < WIDTH; ++w)
+      {
+        for (unsigned char l = 0; l < LENGTH; ++l)
+        {
+          Vector2d tangent_line = get_tangent_line(Vector2d(w, l), ghost_ball_position, pockets[p]);
+          for (unsigned char st = 0; st < NUM_STRENGTHS; ++st)
+          {
+            for (unsigned char sp = 0; sp < NUM_SPINS; ++sp)
+            {
+              if (shot_info_table[w][l][o][p].difficulty > MAX_DIFFICULTY_SHOT_TO_CONSIDER)
+              {
+                continue;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void populate_shot_info_table()
+{
+  populate_shot_info_table_obstructions();
+  populate_shot_info_table_difficulty();
+  populate_shot_path_table();
+}
+
 int main()
 {
+  auto start = std::chrono::system_clock::now();
+
+  eight_ball = Vector2d(DIAMOND_LENGTH, DIAMOND_LENGTH);
+  cue_ball = Vector2d(0.15, DIAMOND_LENGTH * 4);
+  for (unsigned char i = 0; i < 7; ++i)
+  {
+    object_balls.push_back(Vector2d(DIAMOND_LENGTH * 0.2 + DIAMOND_LENGTH * 0.2 * i, DIAMOND_LENGTH * 4));
+    opponent_object_balls.push_back(Vector2d(DIAMOND_LENGTH * 1.6 + DIAMOND_LENGTH * 0.2 * i, DIAMOND_LENGTH * 4.15));
+  }
+
   initialize_pockets();
-  initialize_layout();
-  cout << get_obstructions_on_segment_for_shot(3, object_balls[3], pockets[3]) << endl;
+  populate_ball_to_pocket_obstructions_table();
+  populate_ghost_ball_position_table();
+  populate_shot_info_table();
+
+  cout << "AAAA" << endl
+       << get_tangent_line(Vector2d(2, 1), Vector2d(0, 0), Vector2d(-2, -2)) << endl;
+  auto end = std::chrono::system_clock::now();
+
+  // for (unsigned char o = 0; o < EIGHT_BALL_INDEX + 1; ++o)
+  // {
+  //   for (unsigned char p = 0; p < pockets.size(); ++p)
+  //   {
+  //     for (unsigned char w = 0; w < WIDTH; ++w)
+  //     {
+  //       for (unsigned char l = 0; l < LENGTH; ++l)
+  //       {
+  //         if (w == 0 && l == DIAMOND_LENGTH * 4)
+  //         {
+  //           cout << shot_info_table[w][l][o][p] << endl;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+  std::cout << "finished computation at " << std::ctime(&end_time)
+            << "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
